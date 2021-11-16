c082c18d088ab5c0815e8065dce1f8ce56e856f4
Tom Butler <technoglyf@gmail.com>
2021-11-15 19:06:19 -0800

    updated parallax training files added images added z-index

---
+++ spaceparalax.js
@@ -0,0 +1,10 @@
+
+let stars = document getElementById("stars-space");
+let midMnt = document getElementById("mid-mnt");
+let ground = document getElementById("ground");
+let dome = document getElementById("dome");
+let rocket = document getElementById("rocket");
+let mtn1 = document getElementById("mtn1");
+let moon = document getElementById("moon");
+
+moon.style.border = "3px solid red";
