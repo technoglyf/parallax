971ba7a605bf103865e160357ba2e11e83f7f697
Tom Butler <technoglyf@gmail.com>
2021-11-16 15:48:15 -0800

    added images

--- spaceparalax.js
+++ spaceparalax.js
@@ -1,10 +1,26 @@
+let stars = document.getElementById("stars-space");
+let midMnt = document.getElementById("mid-mnt");
+let ground = document.getElementById("ground");
+let dome = document.getElementById("dome");
+let rocket = document.getElementById("rocket");
+let mtn1 = document.getElementById("mtn1");
+let moon = document.getElementById("moon");
 
-let stars = document getElementById("stars-space");
-let midMnt = document getElementById("mid-mnt");
-let ground = document getElementById("ground");
-let dome = document getElementById("dome");
-let rocket = document getElementById("rocket");
-let mtn1 = document getElementById("mtn1");
-let moon = document getElementById("moon");
 
-moon.style.border = "3px solid red";
+/*
+event listerer
+scrowl
+*/
+
+window.addEventListener("scroll",function(){
+  let value = window.scrollY;
+  console.log(value);
+
+  rocket.style.bottom = 20*value+"px";
+
+  midMnt.style.bottom = .50*value+"px";
+
+  mtn1.style.bottom = -.50*value+"px";
+
+  ground.style.bottom = -.25*value+"px";
+});
